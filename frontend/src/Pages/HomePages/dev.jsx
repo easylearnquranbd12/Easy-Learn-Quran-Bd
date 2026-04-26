1b8c4311f012f59f816d118069095bf387665dcf
Mozammel772 <mozammelhosen.dev@gmail.com>
2026-04-25 23:59:21 -0700

    added vercel

---
+++ frontend/src/Pages/HomePages/dev.jsx
@@ -0,0 +1,4 @@
+// git remote set-url --add --push origin https://github.com/your-first-repo.git
+// git remote set-url --add --push origin https://github.com/easylearnquranbd12/Easy-Learn-Quran-Bd.git
+
+// git push origin main
